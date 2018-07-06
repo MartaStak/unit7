950350657f610844bc0a599ca59297fb5c585ce9
Marta <mstak@softserveinc.com>
2018-07-06 09:28:03 +0000

    initial

--- js/clock.js
+++ js/clock.js
@@ -6,30 +6,35 @@
 //clock.innerHTML = '<b>' + (new Date()).toLocaleTimeString() + '</b>';
 //}
 
- const btnStart = document.querySelector('.start');
- const btnStop = document.querySelector('.stop');
- const display = document.querySelector('.clock');
+// const btnStart = document.querySelector('.start');
+// const btnStop = document.querySelector('.stop');
+// const display = document.querySelector('.clock');
 
- btnStart.addEventListener('click', start);
+// btnStart.addEventListener('click', start);
 
- let startTime;
-function start() {
-     startTime = new Date();
-     setInterval(showTime, 17);
- }
-
- function showTime() {
-     const delta = (new Date()) - startTime;
-     display.innerHTML = '<b>' + delta/1000 + '</b>';
- }
-
-//$('.start').click(start);
-
-//let startTime;
+// let startTime;
 //function start() {
-//    startTime = new Date();
-//    setInterval(showTime, 17);
-//}
+//     startTime = new Date();
+//     setInterval(showTime, 17);
+// }
+
+// function showTime() {
+//     const delta = (new Date()) - startTime;
+//     display.innerHTML = '<b>' + delta/1000 + '</b>';
+// }
+
+$('.start').click(start);
+
+let startTime;
+function start() {
+    startTime = new Date();
+    setInterval(showTime, 17);
+}
+
+function showTime() {
+     const delta = (new Date()) - startTime;
+     $('.clock').html('<b>' + delta/1000 + '</b>');
+}
 
 
 
